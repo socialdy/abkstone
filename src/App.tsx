1e3dffab7c964703bdc52f1870c22345901ea412
socialdy <markuswallner19@hotmail.com>
2025-01-14 21:37:12 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
-
+t
 const App = () => (
   <BrowserRouter>
     <TooltipProvider>
